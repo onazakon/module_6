1c8262041be2b640888ec1526b997c1fa042f85b
onazakon <106270526+onazakon@users.noreply.github.com>
2022-07-26 22:28:35 +0300

    Add files via upload

---
+++ task_6/task_2/sсript.js
@@ -0,0 +1,7 @@
+const consolelog = document.querySelector('#consoleLog'); 
+
+consolelog.addEventListener('click', () => {
+alert('Служит для вывода информации в консоль');
+})
+
+
---
+++ task_6/task_4/script.js
@@ -0,0 +1,16 @@
+var zx = (link);
+const zx = document.querySelector('');
+link.addEventListener('click', () => {
+const zx = prompt('Измени название тут');
+const result = ('').textContent;})
+
+
+
+
+
+
+
+
+
+
+
